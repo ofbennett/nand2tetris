b7d2d8c7e47188c118433e242a0d2df4fc1b0e14
ofbennett <ofbennett@gmail.com>
2020-07-19 11:40:14 +0100

    [Proj 04]: Completes Mult program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,47 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@i
+M=0 // i=0
+
+@sum
+M=0 // sum=0
+
+@2
+M=0 // prod=0
+
+(LOOP)
+
+// Check if i>R1, if so end loop
+@i
+D=M
+@1
+D=M-D
+@ENDLOOP
+D;JLE
+
+// Increase sum by R0
+@sum
+D=M
+@0
+D=D+M
+@sum
+M=D
+
+@i
+M=M+1 // i++
+
+@LOOP
+0;JMP
+(ENDLOOP)
+
+// Assign R2 to final prod value
+@sum
+D=M
+@2
+M=D
+
+// End program
+(END)
+@END
+0;JMP
