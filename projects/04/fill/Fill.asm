f91a3356ba1b71ff811bf76057c43307edabf2b4
ofbennett <ofbennett@gmail.com>
2020-07-19 13:02:50 +0100

    [Proj 04]: Completes Fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,58 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(KEY_CHECK_START)
+// Check if key pressed
+@KBD
+D=M
+@FILL_START
+D;JNE
+@CLEAR_START
+D;JEQ
+
+(KEY_CHECK_CONTINUE)
+// Check if key pressed
+@KBD
+D=M
+@FILL_CONTINUE
+D;JNE
+@CLEAR_CONTINUE
+D;JEQ
+
+(FILL_START)
+@SCREEN
+D=A
+@current
+M=D
+(FILL_CONTINUE)
+@current
+A=M
+M=-1
+@current
+M=M+1
+D=M
+@24576
+D=A-D
+@KEY_CHECK_START
+D;JLE // Check if at end of screen
+@KEY_CHECK_CONTINUE
+0;JMP
+
+(CLEAR_START)
+@SCREEN
+D=A
+@current
+M=D
+(CLEAR_CONTINUE)
+@current
+A=M
+M=0
+@current
+M=M+1
+D=M
+@24576
+D=A-D
+@KEY_CHECK_START
+D;JLE // Check if at end of screen
+@KEY_CHECK_CONTINUE
+0;JMP
